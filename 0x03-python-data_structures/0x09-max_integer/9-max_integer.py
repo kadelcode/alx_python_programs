b64571c1d131f2557e002970953689f964f4f6de
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-08 18:54:02 +0000

    modify code

--- 0x03-python-data_structures/0x09-max_integer/9-max_integer.py
+++ 0x03-python-data_structures/0x09-max_integer/9-max_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    for i in range(len(my_list)):
-        if my_list[i] > my_list[i + 1] and my_list[i] > my_list[i - 1]:
-            return (my_list[i])
+    if my_list == []:
+        return (None)
+    else:
+        max_int = my_list[0]
+
+        for i in range(len(my_list)):
+            print("max_int = {}".format(max_int))
+            if my_list[i] > max_int:
+                max_int = my_list[i]
+
+        return (max_int)
