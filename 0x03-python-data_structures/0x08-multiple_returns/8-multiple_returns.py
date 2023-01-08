7846dbbde197399533ac9fd33452f72c51db8567
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-08 15:57:58 +0000

    code fix

--- 0x03-python-data_structures/0x08-multiple_returns/8-multiple_returns.py
+++ 0x03-python-data_structures/0x08-multiple_returns/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
     if sentence == '':
         first = None
+    else:
+        first = sentence[0]
     multiple_tuple = (length, first)
 
     return (multiple_tuple)
