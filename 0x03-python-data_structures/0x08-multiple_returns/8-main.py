abab90fde436ec8012ae7ac5290550c25770429f
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-08 16:01:34 +0000

    code fix

--- 0x03-python-data_structures/0x08-multiple_returns/8-main.py
+++ 0x03-python-data_structures/0x08-multiple_returns/8-main.py
@@ -6,4 +6,4 @@ sentence1 = ''
 length, first = multiple_returns(sentence)
 length1, first1 = multiple_returns(sentence1)
 print("Length: {:d} - First character: {}".format(length, first))
-print("Length: {} - First character:{}".format(length1, first1))
+print("Length: {} - First character: {}".format(length1, first1))
