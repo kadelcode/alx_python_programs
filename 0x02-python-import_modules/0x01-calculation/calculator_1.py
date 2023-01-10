016f21a3ad66531798bd1ed816dd7441d060df99
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-10 03:57:32 +0000

    Update file

--- 0x02-python-import_modules/0x01-calculation/calculator_1.py
+++ 0x02-python-import_modules/0x01-calculation/calculator_1.py
@@ -34,3 +34,16 @@ def mul(a, b):
         The return value. a / b
     """
     return int(a / b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+
+    return int(a / b)
