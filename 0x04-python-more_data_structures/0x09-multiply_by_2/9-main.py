331d4a39ff5f44016cbdbd1302f7670a8db3cea8
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-13 20:23:37 +0000

    code changes

--- 0x04-python-more_data_structures/0x09-multiply_by_2/9-main.py
+++ 0x04-python-more_data_structures/0x09-multiply_by_2/9-main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import sys
+sys.path.append('../0x06-print_sorted_dictionary')
 multiply_by_2 = __import__('9-multiply_by_2').multiply_by_2
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
--- 0x04-python-more_data_structures/0x09-multiply_by_2/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/0x09-multiply_by_2/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] *= 2
-        return (a_dictionary)
+    new_dict = {x: 2 * i for x, i in a_dictionary.items()} 
+
+    return (new_dict)
