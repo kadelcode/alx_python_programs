50c869573b2a0b653728516f88784118dd6a74fb
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-14 20:44:08 +0000

    Update code

--- 0x04-python-more_data_structures/0x0F-complex_delete/102-main.py
+++ 0x04-python-more_data_structures/0x0F-complex_delete/102-main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import sys
+sys.path.append('../0x06-print_sorted_dictionary')
 complex_delete = __import__('102-complex_delete').complex_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
