bb3d0ad7e29cb688af18d55edda3603c80b8ae33
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-23 14:08:57 +0000

    Update code

--- 0x05-python-exceptions/0x00-safe_print_list/0-main.py
+++ 0x05-python-exceptions/0x00-safe_print_list/0-main.py
@@ -5,8 +5,9 @@ my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
 
+nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
+
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
