da56ad2f65e833dbd6fb937302a8d1525663c0fc
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-27 21:35:16 +0000

    code fix

--- 0x06-python-classes/0x04-square/4-main.py
+++ 0x06-python-classes/0x04-square/4-main.py
@@ -2,7 +2,7 @@
 Square = __import__('4-square').Square
 
 my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(). my_square.size))
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
 my_square.size = 3
 print("Area: {} for size: {}".format(my_square.area(), my_square.size))
