23e99aab5d0788ba39a3266d75bd49dea49ef3d8
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-27 21:27:20 +0000

    setter

---
+++ 0x06-python-classes/0x04-square/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+#4-square.py
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Defines a class Square"""
+    def __init__(self, size=0):
+        """initializes class parameter
+        Args:
+            size (int): The size of a square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def size(self, value)
+        """Return the size of square"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
