ca0624e39e0d9d39653fb1098fb943f5f74c972e
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-27 20:46:22 +0000

    Update code

--- 0x06-python-classes/0x03-square/3-square.py
+++ 0x06-python-classes/0x03-square/3-square.py
@@ -6,13 +6,13 @@
 class Square:
     """Defines a class Square"""
 
-    def __init__(self, __size):
+    def __init__(self, size=0):
         """Initializes class parameter
         Args:
             size (int): The size of the new square
 
         """
-        if not isintance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
