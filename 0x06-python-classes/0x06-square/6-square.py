6b3f74fd34273c9eab0970db3ad40321734eca9e
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-02 19:58:40 +0000

    field initialization

---
+++ 0x06-python-classes/0x06-square/6-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A class that defines a square
+Private instance attribute: size & position
+Public instance method: def area(self)
+                        def my_print(self)
+"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """initializes class attributes"""
+        self.__size = size
+        self.position = position
