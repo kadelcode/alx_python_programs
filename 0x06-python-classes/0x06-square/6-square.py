a9bd87bda6f91d1dc8c8392256305444d2a60012
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-14 20:27:51 +0000

    Update code

--- 0x06-python-classes/0x06-square/6-square.py
+++ 0x06-python-classes/0x06-square/6-square.py
@@ -43,3 +43,14 @@ class Square:
     def area(self):
         """Return the current area of the square"""
         return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
