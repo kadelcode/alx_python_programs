f357063391f97217827acd1fb0d99f5fa5655d5f
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-15 06:27:48 +0000

    __init__ method

---
+++ 0x06-python-classes/100-singly_linked_list/100-singly_linked_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 100-singly_linked_list.py """
+
+
+class Node:
+    """
+    A class that defines a node of a singly linked list
+    """
+    def __init__(self, data, next_node=None):
+        """ Initializes class attributes """
+        self.__data = data
+        self.__next_node = next_node
