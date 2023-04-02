51f2f536b88143d33e0952d385efe0d989e09bc0
Ayodele Kadiri <kadelcode@gmail.com>
2023-04-02 18:44:21 +0000

    code fix

--- 0x06-python-classes/100-singly_linked_list/100-main.py
+++ 0x06-python-classes/100-singly_linked_list/100-main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 SingleLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
-sl1 = SinglyLinkedList()
-sl1.sorted_insert(2)
-sl1.sorted_insert(5)
-sl1.sorted_insert(3)
-sl1.sorted_insert(10)
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
 sll.sorted_insert(1)
 sll.sorted_insert(-4)
 sll.sorted_insert(-3)
