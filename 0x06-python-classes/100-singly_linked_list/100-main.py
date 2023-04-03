802be132509fd28ff4423f43d793bec9569a9a14
Ayodele Kadiri <kadelcode@gmail.com>
2023-04-03 12:01:15 +0000

    spelling fix

--- 0x06-python-classes/100-singly_linked_list/100-main.py
+++ 0x06-python-classes/100-singly_linked_list/100-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-SingleLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
+SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
 
 sll = SinglyLinkedList()
 sll.sorted_insert(2)
