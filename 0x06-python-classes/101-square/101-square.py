d457c2097075b2f61fd961a7f9c426cc3fb2650a
Ayodele Kadiri <kadelcode@gmail.com>
2023-04-03 13:50:12 +0000

    getter & setter for position

--- 0x06-python-classes/101-square/101-square.py
+++ 0x06-python-classes/101-square/101-square.py
@@ -27,3 +27,18 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """Gets the current position of the square"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """sets the position of the square"""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
