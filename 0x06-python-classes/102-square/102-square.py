dbc1373c5dfed119a30b8b4637fd860da693d1b2
Ayodele Kadiri <kadelcode@gmail.com>
2023-04-05 04:49:53 +0000

    area of square

--- 0x06-python-classes/102-square/102-square.py
+++ 0x06-python-classes/102-square/102-square.py
@@ -24,3 +24,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Returns the current ara of the square"""
+        return (self.__size * self.__size)
