f4c2ea2769907546577d916fc0315184e79993f5
Ayodele Kadiri <kadelcode@gmail.com>
2023-04-06 21:55:44 +0000

    lt method

--- 0x06-python-classes/102-square/102-square.py
+++ 0x06-python-classes/102-square/102-square.py
@@ -36,3 +36,7 @@ class Square:
     def __ne__(self, other):
         """Defines the !=comparison to a square"""
         return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Defines the < comparison to a square"""
+        return self.area() < other.area()
