befedce146b3875091ba5842b4ff41ba6143c8b2
Ayodele Kadiri <kadelcode@gmail.com>
2023-04-09 20:27:15 +0000

    addition function

--- 0x07-python-test_driven_development/0x00-add_integer/0-add_integer.py
+++ 0x07-python-test_driven_development/0x00-add_integer/0-add_integer.py
@@ -10,3 +10,11 @@ def add_integer(a, b=98):
 
     if not isinstance(a, int):
         raise TypeError('a must be an integer')
+
+    if type(b) == float:
+        b = int(b)
+
+    if not isinstance(b, int):
+        raise TypeError('b must be an integer')
+
+    return (a + b)
