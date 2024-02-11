5c44db55563b988003fbe178f9cf38ea71c7a717
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-11 14:41:19 +0100

    Hexadecimal printing

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa/4-print_hexa.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+A module that contains a programs that prints all
+numbers from 0 to 98 in decimal and in hexadecimal
+"""
+
+for i in range(99):
+    print(f"{i} = {hex(i)}")
