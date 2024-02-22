6c5fc75846e80d4e1f35ffa6f1ef44b1780a65ab
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-22 17:55:45 +0100

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase/8-uppercase.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+A module containing `uppercase` function
+"""
+
+def uppercase(str):
+    """
+    This function prints a string in uppercase
+    followed by a new line.
+    @str: the string to convert uppercase
+    """
+
+    # check if character in the string, is lowercase
+    for c in str:
+        if ((ord(c) >= 97) and (ord(c) <= 122)):
+            """
+            change the lowercase character to its
+            corresponding uppercase character
+            """
+            c = chr(ord(c) - ord('a') + ord('A'))
+        print(c, end="")
+    print()
+
