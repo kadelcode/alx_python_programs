ec9fa9cea224f962a1b82d280680d2b179d29e3f
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-26 18:30:05 +0100

    a ^ b

---
+++ 0x01-python-if_else_loops_functions/11-pow/11-pow.py
@@ -0,0 +1,12 @@
+"""
+A module that contains a function: `pow`
+"""
+
+
+def pow(a, b):
+    """
+    a function that computes `a` to the power
+    of `b` and return the value
+    """
+
+    return a ** b
