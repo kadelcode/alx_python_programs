b7abc1fc4a80d625771fe5e7dd95e566db2b838f
Ayodele Kadiri <kadelcode@gmail.com>
2024-02-26 18:14:37 +0100

    a + b

---
+++ 0x01-python-if_else_loops_functions/10-add/10-add.py
@@ -0,0 +1,13 @@
+"""
+A module that contains a function: `add`
+"""
+
+
+def add(a,b):
+    """
+    a function that adds two integers and returns the result
+    @a: first integer
+    @b: second integer
+    """
+
+    return a + b
